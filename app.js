a21b38a3e64a508fd2615931ca069a0565f917fc
Kathryn Flinn <flinnk@oregonstate.edu>
2025-08-01 15:28:23 -0700

    rewrite complete --- step 3 draft functionality restored

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
 
-const PORT = 63009;
+const PORT = 50348;
 
 // Database
 const db = require('./database/db-connector');
@@ -54,13 +54,70 @@ app.get('/patients', async function (req, res) {
 app.get('/prescriptions', async function (req, res) {
     try {
         // Create and execute our queries
-        // simple select to get all patient attributes
+        // grabbing prescription info AND the name of the patient each is associated with
         const query1 = `SELECT prescriptionID, concat(Patients.firstName,' ',Patients.lastName) AS patientID, doctorName, dateIssued, numRefills FROM Prescriptions INNER JOIN Patients ON Prescriptions.patientID = Patients.patientID;`;
         const [prescriptions] = await db.query(query1);
 
         // Render the bsg-people.hbs file, and also send the renderer
         //  an object that contains our bsg_people and bsg_homeworld information
         res.render('prescriptions', { prescriptions: prescriptions });
+    } catch (error) {
+        console.error('Error executing queries:', error);
+        // Send a generic error message to the browser
+        res.status(500).send(
+            'An error occurred while executing the database queries.'
+        );
+    }
+});
+
+app.get('/meds', async function (req, res) {
+    try {
+        // Create and execute our queries
+        // simple select to get all meds attributes
+        const query1 = `SELECT medicationID, name, dosageForm, dosageStrength, dosageUnit, quantity FROM Meds;`;
+        const [meds] = await db.query(query1);
+
+        // Render the bsg-people.hbs file, and also send the renderer
+        //  an object that contains our bsg_people and bsg_homeworld information
+        res.render('meds', { meds: meds });
+    } catch (error) {
+        console.error('Error executing queries:', error);
+        // Send a generic error message to the browser
+        res.status(500).send(
+            'An error occurred while executing the database queries.'
+        );
+    }
+});
+
+app.get('/sales', async function (req, res) {
+    try {
+        // Create and execute our queries
+        // simple select to get all meds attributes
+        const query1 = `SELECT saleID, prescriptionID, saleDate, totalAmount FROM Sales`;
+        const [sales] = await db.query(query1);
+
+        // Render the bsg-people.hbs file, and also send the renderer
+        //  an object that contains our bsg_people and bsg_homeworld information
+        res.render('sales', { sales: sales });
+    } catch (error) {
+        console.error('Error executing queries:', error);
+        // Send a generic error message to the browser
+        res.status(500).send(
+            'An error occurred while executing the database queries.'
+        );
+    }
+});
+
+app.get('/prescription-meds', async function (req, res) {
+    try {
+        // Create and execute our queries
+        // grabbing prescriptionMed info w/ the NAME of each med, as opposed to just the id
+        const query1 = `SELECT ID, prescriptionID, Meds.name AS medicationID, quantityFilled, dateFilled, subTotal FROM PrescriptionMeds INNER JOIN Meds ON PrescriptionMeds.medicationID = Meds.medicationID;`;
+        const [prescriptionMeds] = await db.query(query1);
+
+        // Render the bsg-people.hbs file, and also send the renderer
+        //  an object that contains our bsg_people and bsg_homeworld information
+        res.render('prescription-meds', { prescriptionMeds: prescriptionMeds });
     } catch (error) {
         console.error('Error executing queries:', error);
         // Send a generic error message to the browser
